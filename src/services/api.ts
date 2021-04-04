7137f4c48f28f4716bafb81e35585563849b3282
Vinicius Rodrigues <vrsstudy@gmail.com>
2021-04-04 17:02:42 -0400

    https onfire

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://159.89.84.226:3333',
+    baseURL: 'https://mappinfection.vinirodrigues.com',
     headers: {'Access-Control-Allow-Origin': '*'}
 })
 
